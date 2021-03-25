764005318c32812606c040561226618913a3a7bf
Aigul <aigul.sibgatulli@mail.ru>
2021-03-26 01:45:28 +0300

    deployment

--- src/services/sport-service.js
+++ src/services/sport-service.js
@@ -2,7 +2,7 @@ import moment from "moment";
 
 export default class SportService {
   constructor() {
-    this._api = "http://api.football-data.org/v2";
+    this._api = "https://api.football-data.org/v2";
   }
 
   async getResource(url) {
