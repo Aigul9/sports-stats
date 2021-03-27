6c17c44b8fcd7b958f888cf21ff7c52d3bd6ecf6
Aigul <aigul.sibgatulli@mail.ru>
2021-03-27 21:34:14 +0300

    header fix

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,28 +1,17 @@
 import React from "react";
 import { Navbar, Nav } from "rsuite";
 import "./header.css";
-import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Navbar>
       <Navbar.Body className="header">
         <Nav>
-          <Nav.Item>
-            <Link to="/sports-stats/" className="link link-header">
-              Sports statistics
-            </Link>
+          <Nav.Item href="/sports-stats/teams" className="link-header">
+            Sports statistics
           </Nav.Item>
-          <Nav.Item>
-            <Link to="/sports-stats/teams" className="link">
-              Teams
-            </Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Link to="/sports-stats/leagues" className="link">
-              Leagues
-            </Link>
-          </Nav.Item>
+          <Nav.Item href="/sports-stats/teams">Teams</Nav.Item>
+          <Nav.Item href="/sports-stats/leagues">Leagues</Nav.Item>
         </Nav>
       </Navbar.Body>
     </Navbar>
